bb6b9a675ab379874329ef13b7d625c836e928a9
mtl23 <mtl23@njit.edu>
2021-07-29 19:56:38 -0400

    Added graphics.h and sprite.h

--- MainGame.cpp
+++ MainGame.cpp
@@ -4,34 +4,131 @@
 #include <stdio.h>
 #include <string>
 #include <cmath>
-#include "vector.h"
+
 
 
 
 //Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 512;
+const int SCREEN_HEIGHT = 512;
 
 
 
+//Starts up SDL and creates window
+bool init();
 
-int main(int argc, char* args[])
+//Loads media
+bool loadMedia();
+
+//Frees media and shuts down SDL
+void close();
+
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//The window renderer
+SDL_Renderer* gRenderer = NULL;
+
+//The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+
+//The image we will load and show on the screen
+SDL_Surface* gHelloWorld = NULL;
+
+
+bool init()
 {
-	
+	//Initialization flag
+	bool success = true;
 	//Initialize SDL
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
 		printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
-
+		return false;
 	}
 	else
-    {
-	
-		printf("A smal;l step foreward");
-
+	{
+		//Create window
+		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
+		{
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else
+		{
+			//Get window surface
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
+		}
+		printf("A small step forward");
+		return success;
 	}
 
+	return false;
+}
 
+
+bool loadMedia()
+{
+	//Loading success flag
+	bool success = true;
+
+	//Load splash image
+	gHelloWorld = SDL_LoadBMP("m7_map.bmp");
+	if (gHelloWorld == NULL)
+	{
+		printf("Unable to load image %s! SDL Error: %s\n", "02_getting_an_image_on_the_screen/hello_world.bmp", SDL_GetError());
+		success = false;
+	}
+
+	return success;
+}
+
+void close()
+{
+	//Deallocate surface
+	SDL_FreeSurface(gHelloWorld);
+	gHelloWorld = NULL;
+
+	//Destroy window
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
+}
+
+int main(int argc, char* args[])
+{
 	
+	
+	//Start up SDL and create window
+	if (!init())
+	{
+		printf("Failed to initialize!\n");
+	}
+	else
+	{
+		//Load media
+		if (!loadMedia())
+		{
+			printf("Failed to load media!\n");
+		}
+		else
+		{
+			//Apply the image
+			SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+
+			//Update the surface
+			SDL_UpdateWindowSurface(gWindow);
+			//Wait two seconds
+			SDL_Delay(2000);
+		}
+	}
+
+	//Free resources and close SDL
+	close();
+
 	return 0;
 }
---
+++ sprite.h
@@ -0,0 +1,82 @@
+#ifndef __SPRITE_H__
+#define __SPRITE_H__
+#include <SDL.h>
+#include <SDL_image.h>
+#include "vector.h"
+/**
+* @basic Sprite struct
+**/
+//typedef struct {
+//
+//	float y; /**<the y component of the sprite in local coordinates */
+//	float x; /**<the x component of the center  of the sprite */
+//	float h; /**<the h component of the center of the sprite   */
+//	float w; /**<the w component of the sprite */
+//	float refcount;
+//	float inuse = 0; //used as a boolean
+//	char filename[24];// assidned names of the sprite
+//	SDL_Renderer* renderer;// the desired renderer to draw to
+//	int frame;
+//	int frameW;
+//	int frameH;
+//	int framesPerLine;
+//	float angle;
+//	Vector3D color;
+//	SDL_Texture* image;
+//
+//}Sprite_M;
+
+
+
+typedef struct
+
+{
+	int refcount;
+	char filename[32];
+	SDL_Texture* image;
+	int imageW;
+	int imageH;
+	float angle;
+	int frame;
+	int frameW;
+	int frameH;
+	int framesPerLine;
+	Vector3D color;
+
+}Sprite_M;
+
+
+/**
+*
+*@brief creates the sptite system, should be used once at game start
+*/
+void InitSpriteSystem();
+
+/**
+*
+*@brief closes the sptite system, should be used once at game end
+*/
+void CloseSpriteSystem();
+
+/**
+*
+*@brief allocates a space in the master Sprite-List for a sprite of a given filename (PNG)
+*@param filename: the filename of the srpite associated with the entity
+*@param sizeX: the final width of the sprite when its drawn
+*@param sizeX: the final height of the sprite when its drawn
+*@return Sprite_M: returns a Sprite_M object so that it can be saved for later. This is necessary to use spriteDraw()
+*/
+Sprite_M* spriteLoad(char* filename, int sizeX, int sizeY);
+
+
+Sprite_M* spriteLoadNoEdits(char* filename, int sizeX, int sizeY);
+/**
+*
+*@brief frees a single sprite back to memory
+ @param *sprite: pointer to the sprite that you wish to free
+*/
+
+void spriteColorBlendDraw(Sprite_M* sprite, SDL_Renderer* renderer, int frame, Vector2D position);
+void spriteDrawNoBlend(Sprite_M* sprite, SDL_Renderer* renderer, int frame, Vector2D position);
+void spriteFree(Sprite_M* sprite);
+#endif
