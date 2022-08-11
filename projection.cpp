cad98586e6eab53c2f4980d1b0b3e8275b7968ea
Mat L <mtl23@njit.edu>
2022-08-11 14:29:34 -0400

    time to do inputs

---
+++ projection.cpp
@@ -0,0 +1,5 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include "vector.h"
+#include "sprite.h"
+#include "simple_logger.h"
---
+++ projection.h
@@ -0,0 +1,11 @@
+#ifndef __PROJECTOIN_H__
+#define __PROJECTOIN_H__
+
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include "vector.h"
+#include "sprite.h"
+#include "simple_logger.h"
+
+#endif
